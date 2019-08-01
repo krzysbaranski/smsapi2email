7092c9367a4394133f043f4284dbd6cea5dc81ad
Krzysztof Baranski <baranski5@gmail.com>
2019-08-01 22:12:26 +0200

    set -x in test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x -o pipefail
+set -ex -o pipefail
 
 RUN=$(curl --silent "http://localhost:8080/sms.do?username=user&from=smsapi&to=666777888&message=some_message")
 RETURN_CODE=$(echo ${RUN} | cut -c1-3)
@@ -7,6 +7,7 @@ if [ "${RETURN_CODE}" != "OK:" ]; then
   echo "Invalid return code \"${RETURN_CODE}\""
   exit 1
 fi
+# check output using MailHog API
 BODY=$(curl --silent 'http://localhost:8025/api/v2/search?kind=containing&query=666777888' | jq -r .items[0].Content.Body)
 if [ "${BODY}" != "some_message" ]; then
   echo "Invalid message body \"${BODY}\""
